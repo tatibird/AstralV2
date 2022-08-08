0e557bb45477c8e33e43c0943c78ef684c4e5211
Mastadawn <68205491+Mastadawn@users.noreply.github.com>
2022-08-07 20:27:08 -0400

    fixed getnearby players ignore self option

--- Utilities.lua
+++ Utilities.lua
@@ -2,13 +2,13 @@ Utility = {
     Predict = function(player)
         return player.Character.HumanoidRootPart.Position + Vector3.new(player.Character.HumanoidRootPart.Velocity.X/10,player.Character.HumanoidRootPart.Velocity.Y/50,player.Character.HumanoidRootPart.Velocity.Z/10)
     end,
-    GetNearbyPlayers = function(range,self)
+    GetNearbyPlayers = function(range,addself)
         local nearby = {}
         for _,Player in pairs(game:GetService("Players"):GetPlayers()) do
             if Player.Character and Player.Character.PrimaryPart then
                 if (Player.Character.PrimaryPart.Position - game.Players.LocalPlayer.Character.PrimaryPart.Position).Magnitude <= range then
                     if Player.UserId == game.Players.LocalPlayer.UserId then
-                        if self == true then
+                        if addself == true then
                             table.insert(nearby,Player)
                         end
                     else
