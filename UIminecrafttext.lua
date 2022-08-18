b9cc9ec17a5a1412c2aa1f0568eab517a1cd666e
Mastadawn <68205491+Mastadawn@users.noreply.github.com>
2022-08-18 14:21:32 -0400

    Update UIminecrafttext.lua

--- UIminecrafttext.lua
+++ UIminecrafttext.lua
@@ -1,3 +1,12 @@
+if not isfolder("fonts") then
+	makefolder("fonts")
+end
+
+if not readfile("fonts/minecraft.ttf") then
+	local MinecraftFont = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mastadawn/AstralV2/main/Minecraft.ttf", true))()
+	makefile("fonts/minecraft.ttf",MinecraftFont)
+end
+
 getgenv().GUI = Instance.new("ScreenGui", game:GetService"CoreGui")
 getgenv().HUDColor = Color3.fromRGB(150, 0, 240)
 getgenv().HUDAnimations = true
